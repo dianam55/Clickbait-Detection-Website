c35b064db1cbf39bc22927fb17975abd83a22031
dianam55 <dianamuresan55@yahoo.com>
2025-07-21 20:14:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -530,8 +530,9 @@ def sentiment_hu():
     
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    #port = int(os.environ.get("PORT", 10000))
+    #app.run(host="0.0.0.0", port=port)
+    app.run(debug=False, host='0.0.0.0', port=8080)
 
     
 
